ddd8d316415d893d3515bf109df34f3c312ee246
ZcWang <15263179+wangzc1111@user.noreply.gitee.com>
2026-02-20 19:41:10 +0800

    feat(login): add login page component

--- frontend/client/src/pages/Login.tsx
+++ frontend/client/src/pages/Login.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Loader2 } from 'lucide-react';
-import type { Credentail } from '@/types';
+import type { Credentail } from '@/types' ;
 import { 
     useNavigate 
 } from 'react-router-dom';
