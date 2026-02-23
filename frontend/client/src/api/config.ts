8b10a9e3756229a80a44385e14954dcfb2af4284
ZcWang <15263179+wangzc1111@user.noreply.gitee.com>
2026-02-23 11:57:24 +0800

    feat(api):connect with backend

--- frontend/client/src/api/config.ts
+++ frontend/client/src/api/config.ts
@@ -5,8 +5,8 @@ import { useUserStore } from '@/store/useUserStore';
 // axios.defaults.baseURL = 'http://localhost:5173/api'
 // axios.defaults.baseURL = 'http://localhost:3000/api'
 const instance = axios.create({
-//   baseURL: 'http://localhost:3000/api'
-  baseURL: 'http://localhost:5173/api'
+  baseURL: 'http://localhost:3000/api'
+  // baseURL: 'http://localhost:5173/api'
 })
  instance.interceptors.request.use(config => {
   // 如果 store 中有 accessToken 字段，则添加到请求头
