bc7b17bc4fe70c37d4f505fe72b96ddd9845166a
ZcWang <15263179+wangzc1111@user.noreply.gitee.com>
2026-02-23 11:56:00 +0800

    feat(posts):add posts with psql

--- backend/server/src/app.module.ts
+++ backend/server/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { AIModule } from './ai/ai.module';
+import { PostsModule } from './posts/posts.module';
+
+
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthModule,AIModule,PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
